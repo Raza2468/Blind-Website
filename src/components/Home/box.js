b006b5e504baf35c7ddec66d2abe6627d8394d59
Raza2468 <faizeraza2468@gamil.com>
2021-08-23 01:13:10 +0500

    add4Main

--- src/components/Home/box.js
+++ src/components/Home/box.js
@@ -156,7 +156,200 @@ export default function Box() {
 
             </div>
 
+            {/* What CLIENT ARE SAYING.... */}
+            <div class="WhatClients" >
+                <p class="p-3 "><b>WHAT CLIENTS ARE SAYING....</b></p>
+            </div>
 
+            <div>
+                <div class="row">
+                    {/* First Card MAin*/}
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-4" id="maincard">
+                        <Fade left>
+                            <div id="cardimg">
+                                <img src="https://thumbs.dreamstime.com/b/profile-photo-placeholder-icon-design-vector-illustration-206572730.jpg" id="maincardimg" />
+                            </div>
+                            <div id="maintext">
+                                <p><b>I,m obsessed with the Mineral Bronzer<br />
+                                    by Alima pure. This stuff brings out <br />
+                                    cheakbones I never Knew I had!<br /><br />
+                                    Jessica Smith</b>
+                                </p>
+                            </div>
+                        </Fade>
+                    </div>
+
+                    {/* 2nd Card  Main*/}
+
+                    <div class="col-sm-4" id="maincard">
+                        <Fade right>
+                            <div id="cardimg">
+                                <img src="https://thumbs.dreamstime.com/b/profile-photo-placeholder-icon-design-vector-illustration-206572730.jpg" id="maincardimg" />
+                            </div>
+                            <div id="maintext">
+                                <p><b>I,m obsessed with the Mineral Bronzer<br />
+                                    by Alima pure. This stuff brings out <br />
+                                    cheakbones I never Knew I had!<br /><br />
+                                    Jessica Smith</b>
+                                </p>
+                            </div>
+                        </Fade>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+
+                <div class="row">
+                    {/* 3rd Card Main */}
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-4" id="maincard">
+                        <Fade left>
+                            <div id="cardimg">
+                                <img src="https://thumbs.dreamstime.com/b/profile-photo-placeholder-icon-design-vector-illustration-206572730.jpg" id="maincardimg" />
+                            </div>
+                            <div id="maintext">
+                                <p><b>I,m obsessed with the Mineral Bronzer<br />
+                                    by Alima pure. This stuff brings out <br />
+                                    cheakbones I never Knew I had!<br /><br />
+                                    Jessica Smith</b>
+                                </p>
+                            </div>
+                        </Fade>
+                    </div>
+
+                    {/* 4nd Card Main */}
+
+                    <div class="col-sm-4" id="maincard">
+                        <Fade right>
+                            <div id="cardimg">
+                                <img src="https://thumbs.dreamstime.com/b/profile-photo-placeholder-icon-design-vector-illustration-206572730.jpg" id="maincardimg" />
+                            </div>
+                            <div id="maintext">
+                                <p><b>I,m obsessed with the Mineral Bronzer<br />
+                                    by Alima pure. This stuff brings out <br />
+                                    cheakbones I never Knew I had!<br /><br />
+                                    Jessica Smith</b>
+                                </p>
+                            </div>
+                        </Fade>
+                    </div>
+                    <div class="col-sm-2"></div>
+                </div>
+            </div>
+            {/* Footer */}
+            <div>
+                {/* <!-- Footer --> */}
+                <footer class="page-footer font-small indigo">
+
+                    {/* <!-- Footer Links --> */}
+                    <div class="container text-center text-md-left">
+
+                        {/* <!-- Grid row --> */}
+                        <div class="row">
+
+                            {/* <!-- Grid column --> */}
+                            <div class="col-md-3 mx-auto">
+
+                                {/* <!-- Links --> */}
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Logo</h5>
+
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            <hr class="clearfix w-100 d-md-none" />
+
+                            {/* <!-- Grid column --> */}
+                            <div class="col-md-3 mx-auto">
+
+                                {/* <!-- Links --> */}
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Home</h5>
+
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href="#!">Link 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 4</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            <hr class="clearfix w-100 d-md-none" />
+
+                            {/* <!-- Grid column --> */}
+                            <div class="col-md-3 mx-auto">
+
+                                {/* <!-- Links --> */}
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">PolicY</h5>
+
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href="#!">Link 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 4</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                            <hr class="clearfix w-100 d-md-none" />
+
+                            {/* <!-- Grid column --> */}
+                            <div class="col-md-3 mx-auto">
+
+                                {/* <!-- Links --> */}
+                                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">FOllow US ON</h5>
+
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href="#!">Link 1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Link 4</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            {/* <!-- Grid column --> */}
+
+                        </div>
+                        {/* <!-- Grid row --> */}
+
+                    </div>
+                    {/* <!-- Footer Links -->
+                    // https://bbbootstrap.com/snippets/footer-template-social-media-icons-14199861
+
+                                <!-- Copyright --> */}
+                    <div class="footer-copyright text-center py-3">© Faizeraza2468:
+                        {/* <a href="https://mdbootstrap.com/"> MDBootstrap.com</a> */}
+                    </div>
+                    {/* <!-- Copyright --> */}
+
+                </footer>
+                {/* <!-- Footer --> */}
+            </div>
 
         </div>
     )
